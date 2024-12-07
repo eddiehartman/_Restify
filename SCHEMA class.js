da4f1cc158cf428616c5e855df7cc6bf36a8552f
Eddie Hartman <eddiehartman@gmail.com>
2024-12-07 20:22:41 +0100

    I have the form working!!

--- SCHEMA class.js
+++ SCHEMA class.js
@@ -1,29 +1,28 @@
-// The SCHEMA class is handy for requesting and parsing OpenAPI schema to a JS object for analysis
-var SCHEMA = function(schemaStr, format) {
-	_WHERE = "SCHEMA class";	// Global variable available for error reporting
+function SCHEMA(schemaStr, format) {
+	_WHERE = "SCHEMA class";	/* Global variable available for error reporting */
 	this.name = "SCHEMA";
 	
-	// Main init logic after method definitions
+	/* Main init logic after method definitions
 
-    // The name says it all. Returns a JS Obj
-    this.parseOpenAPISchema = function(openApiSchema) {
-		// Not sure if this is necessary
+       The name says it all. Returns a JS Obj */
+       
+    function parseOpenAPISchema(openApiSchema) {
         openApiSchema = openApiSchema.trim();
 
-        // Load Jackson libraries
+        /* Load Jackson libraries */
         var ObjectMapper = com.fasterxml.jackson.databind.ObjectMapper;
         var objectMapper = new ObjectMapper();
 
-        // Check if YAML and if so, convert to Json
+        /* Check if YAML and if so, convert to Json */
         if ("YAML".equalsIgnoreCase(this.format)) {
-            // Use SnakeYAML to convert the YAML to a map that the ObjectMapper can read
+            /* Use SnakeYAML to convert the YAML to a map that the ObjectMapper can read */
             var Yaml = Packages.org.yaml.snakeyaml.Yaml;
-            // Create instances of Yaml and ObjectMapper
+            /* Create instances of Yaml and ObjectMapper */
             var yaml = new Yaml();
-            // Parse YAML into a Java Map
+            /* Parse YAML into a Java Map */
             var map = yaml.load(openApiSchema);
 
-            // Convert Java Map to JSON string
+            /* Convert Java Map to JSON string */
             var jsonString = objectMapper.writeValueAsString(map)
         } else {
             jsonString = openApiSchema;
@@ -33,8 +32,8 @@ var SCHEMA = function(schemaStr, format) {
         return this.schemaObj
     }
 
-    // Returns the child elements (JS Obj) of the specified path
-    // Root level if no argument or blank text passed
+    /* Returns the child elements (JS Obj) of the specified path
+       Root level if no argument or blank text passed */
     this.getChildren = function(path) { 
         var children = {};
         var parent;
@@ -61,7 +60,7 @@ var SCHEMA = function(schemaStr, format) {
         return children;
     }
     
-    // Main init logic
+    /* Main init logic */
     this.schemaStr = schemaStr;
     this.format = format || "YAML";
     this.schemaObj = this.parseOpenAPISchema(schemaStr)
